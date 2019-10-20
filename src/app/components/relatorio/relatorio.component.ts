086c69ebc2859a242b51ce984513332e7319e837
daniellrs3 <daniellrs3@hotmail.com>
2019-10-20 19:30:03 -0300

    Subindo alteracoes no relatorio

--- src/app/components/relatorio/relatorio.component.ts
+++ src/app/components/relatorio/relatorio.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Input,
+  EventEmitter,
+  ViewChild,
+  ElementRef,
+  Renderer2
+} from "@angular/core";
+import { environment } from "src/environments/environment";
+import * as jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 
 @Component({
   selector: "app-relatorio",
@@ -7,9 +18,38 @@ import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 })
 export class RelatorioComponent implements OnInit {
   @Input("report") report: any;
-  @Output() export = new EventEmitter();
+  @ViewChild("reportContent", { static: false }) reportEl: ElementRef;
+  pathBase: string = environment.api;
 
-  constructor() {}
+  constructor(private renderer: Renderer2) {}
 
   ngOnInit() {}
+
+  export() {
+    this.renderer.removeClass(this.reportEl.nativeElement, "body");
+
+    html2canvas(this.reportEl.nativeElement, { allowTaint: true }).then(
+      function(canvas) {
+        var imgData = canvas.toDataURL("image/jpeg", 1.0);
+        var imgWidth = 210;
+        var pageHeight = 295;
+        var imgHeight = (canvas.height * imgWidth) / canvas.width;
+        var heightLeft = imgHeight;
+        var doc = new jsPDF("p", "mm");
+        var position = 0;
+
+        doc.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+
+        while (heightLeft >= 0) {
+          position = heightLeft - imgHeight;
+          doc.addPage();
+          doc.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+          heightLeft -= pageHeight;
+        }
+        doc.save("file.pdf");
+      }
+    );
+    this.renderer.addClass(this.reportEl.nativeElement, "body");
+  }
 }
--- src/app/components/relatorio/relatorio.module.ts
+++ src/app/components/relatorio/relatorio.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RelatorioComponent } from "./relatorio.component";
 import { IconsModule } from "src/app/icons/icons.module";
+import { PdfViewerModule } from "ng2-pdf-viewer";
 
 @NgModule({
   declarations: [RelatorioComponent],
-  imports: [CommonModule, IconsModule],
+  imports: [CommonModule, IconsModule, PdfViewerModule],
   exports: [RelatorioComponent]
 })
 export class RelatorioModule {}
